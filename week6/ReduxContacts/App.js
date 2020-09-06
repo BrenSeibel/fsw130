15cd73e75717c9c3fce87d69f559743dceafc641
BrenSeibel <brens.seibel@gmail.com>
2020-09-05 22:00:05 -0400

    test

---
+++ week6/ReduxContacts/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {connect} from "react-redux"
+
+function App(props) {
+    return (
+        <div>
+            <h1>CONTACT LIST</h1>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default App
